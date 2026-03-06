903d960e25c11709440bd5725275897abc291e45
Thomas Roche-Rebelle <thomas.roche-rebelle@epitech.eu>
2026-03-06 15:08:40 +0100

    fix: baseUrl configuration into repository

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: "https://koudmain.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/Koudmain-documentation",
   trailingSlash: undefined,
 
   // GitHub pages deployment config.
