7881a9a499cd887e617720cea13137f70a88d856
Thomas Roche-Rebelle <thomas.roche-rebelle@epitech.eu>
2026-03-25 16:41:49 +0100

    add: New domain name for documentation

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://koudmain.github.io",
+  url: "https://docs.koudmain.fr",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/Koudmain-documentation",
+  baseUrl: "/",
   trailingSlash: undefined,
 
   // GitHub pages deployment config.
